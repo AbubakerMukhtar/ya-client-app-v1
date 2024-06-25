6599f017b26d2f711af236340e3d2d6208398a42
ahsan292 <shakoorahsan7@gmail.com>
2024-06-25 15:45:01 +0500

    Summary Page Design

--- src/clientApp/components/summary/Summary.js
+++ src/clientApp/components/summary/Summary.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import './Summary.css';
-import SummaryHeader from '../summaryHeader/SummaryHeader';
-import SummaryCard from '../summaryCard/SummaryCard';
+import React from "react";
+import "./Summary.css";
+import SummaryHeader from "../summaryHeader/SummaryHeader";
+import SummaryCard from "../summaryCard/SummaryCard";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 
 const Summary = (props) => {
   return (
@@ -31,7 +33,131 @@ const Summary = (props) => {
               </p>
             </div>
           </SummaryCard>
-          <SummaryCard companyTitle="Directors/Shareholder"></SummaryCard>
+          <SummaryCard companyTitle="Directors/Shareholder">
+            <div className="company-holders">
+              <div className="company-holder">
+                <h6>Abubaker Mukhtar</h6>
+                <div className="company-holder-actions">
+                  <p>Director</p>
+                  <a href="" className="editicon">
+                    <EditOutlinedIcon className="editIcon" />
+                  </a>
+                  <a href="" className="editicon">
+                    <CloseOutlinedIcon className="editIcon" />
+                  </a>
+                </div>
+              </div>
+              <div className="company-holder">
+                <h6>Abubaker Mukhtar</h6>
+                <div className="company-holder-actions">
+                  <p>Director</p>
+                  <a href="" className="editicon">
+                    <EditOutlinedIcon className="editIcon" />
+                  </a>
+                  <a href="" className="editicon">
+                    <CloseOutlinedIcon className="editIcon" />
+                  </a>
+                </div>
+              </div>
+              <div className="company-holder">
+                <h6>Abubaker Mukhtar</h6>
+                <div className="company-holder-actions">
+                  <p>Director</p>
+                  <a href="" className="editicon">
+                    <EditOutlinedIcon className="editIcon" />
+                  </a>
+                  <a href="" className="editicon">
+                    <CloseOutlinedIcon className="editIcon" />
+                  </a>
+                </div>
+              </div>
+              <div className="company-holder">
+                <h6>Abubaker Mukhtar</h6>
+                <div className="company-holder-actions">
+                  <p>Director</p>
+                  <a href="" className="editicon">
+                    <EditOutlinedIcon className="editIcon" />
+                  </a>
+                  <a href="" className="editicon">
+                    <CloseOutlinedIcon className="editIcon" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </SummaryCard>
+        </div>
+        <div className="summary-content">
+          <SummaryCard companyTitle="Numbered">
+            <div className="company-detail">
+              <p>
+                <span>Company Name</span>
+              </p>
+              <p>
+                Jurisdiction: <span> NOT SURE </span>
+              </p>
+              <p>
+                Province: <span> NS </span>
+              </p>
+              <p>
+                Main Business: <span> 123456789 </span>
+              </p>
+              <p>
+                Address: <span> D1 Johar Town, 346, Lahore, NS, Canada </span>
+              </p>
+            </div>
+          </SummaryCard>
+          <SummaryCard companyTitle="Directors/Shareholder">
+            <div className="company-holders">
+              <div className="company-holder">
+                <h6>Abubaker Mukhtar</h6>
+                <div className="company-holder-actions">
+                  <p>Director</p>
+                  <a href="" className="editicon">
+                    <EditOutlinedIcon className="editIcon" />
+                  </a>
+                  <a href="" className="editicon">
+                    <CloseOutlinedIcon className="editIcon" />
+                  </a>
+                </div>
+              </div>
+              <div className="company-holder">
+                <h6>Abubaker Mukhtar</h6>
+                <div className="company-holder-actions">
+                  <p>Director</p>
+                  <a href="" className="editicon">
+                    <EditOutlinedIcon className="editIcon" />
+                  </a>
+                  <a href="" className="editicon">
+                    <CloseOutlinedIcon className="editIcon" />
+                  </a>
+                </div>
+              </div>
+              <div className="company-holder">
+                <h6>Abubaker Mukhtar</h6>
+                <div className="company-holder-actions">
+                  <p>Director</p>
+                  <a href="" className="editicon">
+                    <EditOutlinedIcon className="editIcon" />
+                  </a>
+                  <a href="" className="editicon">
+                    <CloseOutlinedIcon className="editIcon" />
+                  </a>
+                </div>
+              </div>
+              <div className="company-holder">
+                <h6>Abubaker Mukhtar</h6>
+                <div className="company-holder-actions">
+                  <p>Director</p>
+                  <a href="" className="editicon">
+                    <EditOutlinedIcon className="editIcon" />
+                  </a>
+                  <a href="" className="editicon">
+                    <CloseOutlinedIcon className="editIcon" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </SummaryCard>
         </div>
       </div>
     </>
