d5d298802f307f602bb6909f1594a6751b43bf5f
AbubakerMukhtar <abubakar.mukhtar.3915@gmail.com>
2024-06-26 11:34:44 +0500

    stepper style

--- src/clientApp/components/biStepper/HStepper.js
+++ src/clientApp/components/biStepper/HStepper.js
@@ -12,27 +12,27 @@ const steps = [
   'Summary'
 ];
 
-export default function HStepper ({currentStep, numberOfSteps}) {
+export default function HStepper({ currentStep, numberOfSteps }) {
   const activeStepStyle = (index) => currentStep === index ? "active" : "";
   const isFinalStep = (index) => index === numberOfSteps - 1;
 
   return (
     <div className="horizontal-stepper">
-      <div className="steps">
-      {Array.from({length: numberOfSteps}).map((_, index) => (
-        <React.Fragment key={index}>
-          <div className={`step ${activeStepStyle(index)}`}>
-            {currentStep === index && (
-              <>
-                <div className="inner-dot"></div>
-              </>
-            )}
-          <div className={`step-label ${activeStepStyle(index)}`}>{steps[index]}</div>
-          </div>
-          {isFinalStep(index) ? null : <div className='step-line'></div>}
-        </React.Fragment>
-      ))}
+      <div className="steps-row">
+        {Array.from({ length: numberOfSteps }).map((_, index) => (
+          <React.Fragment key={index}>
+            <div className={`step ${activeStepStyle(index)}`}>
+              {currentStep === index && <div className="inner-dot"></div>}
+            </div>
+            {isFinalStep(index) ? null : <div className='step-line'></div>}
+          </React.Fragment>
+        ))}
+      </div>
+      <div className="labels-row">
+        {Array.from({ length: numberOfSteps }).map((_, index) => (
+          <span key={index} className={`step-label ${activeStepStyle(index)}`}>{steps[index]}</span>
+        ))}
       </div>
     </div>
-  )
+  );
 }
